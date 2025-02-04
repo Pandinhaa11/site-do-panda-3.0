bb9a30a01446319e74dd9df587c1468ef9a15dfc
Pandinhaa11 <maraujosoares936@gmail.com>
2025-02-04 20:19:34 -0200

    novas coisass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,8 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Meu nome é Miguel, sou programador, DEV, e etc.
+      <a href="https://github.com/Pandinhaa11" target="_blank" rel="noopener">Meu github</a>
     </h3>
   </div>
 </template>
